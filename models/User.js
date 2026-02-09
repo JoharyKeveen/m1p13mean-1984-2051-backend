82e482b661bc4b4a79067925ce36ec8b0a2bfb9a
Dylan GHC <dylan@gethumancall.fr>
2026-02-09 11:23:46 +0300

    Architecture

--- models/User.js
+++ models/User.js
@@ -1,43 +1,21 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: [validator.isEmail, 'Email invalide']
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-        select: false
-    },
-    pdp: { 
-        type: String
-    },
-    role: {
-        type: String,
-        enum: ['admin', 'shop','user'],
-        default: 'user'
-    },
-    status: {
-        type: String,
-        enum: ['active', 'inactive'],
-        default: 'active'
-    },
-    phone: { 
-        type: String
-    }
-}, { timestamps: true });
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-UserSchema.pre('save', async function() {
-  if (!this.isModified('password')) return;
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return; // si le mdp n'est pas modifié, on sort
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-module.exports = mongoose.model('User', UserSchema);
+// Vérifier le mot de passe
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,31 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-const app = express();
-const PORT = process.env.PORT || 5000;
-const session = require('express-session');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
-// Middleware
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(session({
-    name: 'auth_session',
-    secret: 'ConnectionUserSecret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60
-    }
-}));
 
-// Connexion à MongoDB
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connecté"))
-    .catch(err => console.log(err));
+connectDB();
 
-// Routes
-app.use('/auth', require('./routes/userRoutes'));
-app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
+
+app.use("/api/users", userRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+// Lancement du serveur
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
